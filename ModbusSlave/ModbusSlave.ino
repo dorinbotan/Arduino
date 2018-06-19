4bc9edb2fccef428d23dc4b1ba0c4731ac16c44f
dorinbotan <dorinbotan@mail.ru>
2018-06-19 08:20:50 +0200

    .

--- ModbusSlave/ModbusSlave.ino
+++ ModbusSlave/ModbusSlave.ino
@@ -1,8 +1,24 @@
-#define BROADCAST 0x00
-#define ADDRESS   0xCB
+#define BROADCAST_ADDRESS 0x00
+//#define SLAVE_ADDRESS     0xCB
+#define SLAVE_ADDRESS     0xDE
 
 int buffer_size = 0;
 unsigned char *input_buffer;
+unsigned long my_delay = 0;
+
+void processFrame(unsigned char *frame, int frame_size)
+{
+  unsigned char addr = frame[0];
+
+  if(addr == SLAVE_ADDRESS || 
+     addr == BROADCAST_ADDRESS)
+  {
+    unsigned char instr = frame[1];
+    
+    Serial.println("Slave accessed");
+    Serial.println(instr, HEX);
+  }
+}
 
 void setup()
 {
@@ -12,17 +28,26 @@ void setup()
 }
 
 void loop() 
-{
+{  
   while(Serial.available() > 0) 
   {
-    *(input_buffer + buffer_size) = (char)Serial.read();
+    input_buffer[buffer_size] = (char)Serial.read();
     buffer_size++;
+    
+    my_delay = 0;
   }
-  
+
   if(buffer_size != 0)
   {
-    buffer_size = 0;
-
-//    Serial.print(input_buffer[0], HEX);
+    // Frame received
+    if(my_delay > 500) // (~190 delay between bytes, ~112k = ~100ms)
+    {
+      processFrame(input_buffer, buffer_size);
+      
+      my_delay = 0;
+      buffer_size = 0;
+    }
   }
+
+  my_delay++;
 }
